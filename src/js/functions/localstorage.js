0508fc7a467aad90596175ebdebfed29ffac1974
dhauke89 <18323741+dhauke89@users.noreply.github.com>
2024-12-13 14:31:14 -0500

    Add default projects JSON

--- src/js/functions/localstorage.js
+++ src/js/functions/localstorage.js
@@ -11,20 +11,23 @@ const saveProjects = (projectList) => {
 }
 
 // Pull all saved tasks & projects from JSON and parse.
-const defaultJSON = '[{"_title":"Demo Task","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T03:59:56.431Z","_priority":3,"_complete":false,"_project":null},{"_title":"Demo Task2","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:02:30.925Z","_priority":2,"_complete":false,"_project":null},{"_title":"Demo Task3","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:02:54.846Z","_priority":1,"_complete":false,"_project":null},{"_title":"Demo Task4","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:03:00.333Z","_priority":2,"_complete":false,"_project":null},{"_title":"Demo Task5","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:03:11.652Z","_priority":3,"_complete":false,"_project":null},{"_title":"Demo Task6","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:03:19.270Z","_priority":3,"_complete":false,"_project":null},{"_title":"Demo Task7","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:04:15.037Z","_priority":3,"_complete":false,"_project":null},{"_title":"Demo Task8","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:04:50.981Z","_priority":3,"_complete":false,"_project":null}]'
+const defaultTasksJSON = '[{"_title":"Demo Task","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T03:59:56.431Z","_priority":3,"_complete":false,"_project":null},{"_title":"Demo Task2","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:02:30.925Z","_priority":2,"_complete":false,"_project":null},{"_title":"Demo Task3","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:02:54.846Z","_priority":1,"_complete":false,"_project":null},{"_title":"Demo Task4","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:03:00.333Z","_priority":2,"_complete":false,"_project":null},{"_title":"Demo Task5","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:03:11.652Z","_priority":3,"_complete":false,"_project":null},{"_title":"Demo Task6","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:03:19.270Z","_priority":3,"_complete":false,"_project":null},{"_title":"Demo Task7","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:04:15.037Z","_priority":3,"_complete":false,"_project":null},{"_title":"Demo Task8","_description":"This is a task for demonstration purposes","_dueDate":"2024-12-10T04:04:50.981Z","_priority":3,"_complete":false,"_project":null}]'
 
 const loadTasks = () => {
     if(!localStorage.getItem('saved-tasks')){
         console.log('No tasks found in local storage. Default task loaded.');
-        return JSON.parse(defaultJSON);
+        return JSON.parse(defaultTasksJSON);
     }else{
         return JSON.parse(localStorage.getItem('saved-tasks'));
     }
 }
 
+const defaultProjectsJSON = '["Test","test2","test3"]'
+
 const loadProjects = () => {
     if(!localStorage.getItem('saved-projects')){
-        throw console.error('No projects found in local storage.');
+        console.log('No projects found in local storage. Loading default projects');
+        return JSON.parse(defaultProjectsJSON);
     }else{
         return JSON.parse(localStorage.getItem('saved-projects'));
     }

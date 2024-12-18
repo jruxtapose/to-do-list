c65a07edec6da916f350b25625e0c324cc45d889
dhauke89 <18323741+dhauke89@users.noreply.github.com>
2024-12-18 16:31:11 -0500

    Split out project button rendering into seperate function.

--- src/js/index.js
+++ src/js/index.js
@@ -37,32 +37,37 @@ overDueTasksButton.addEventListener('click', () => {
     renderCurrentTasks();
 })
 
+const renderProject = (project) => {
+    const projectDisplay = document.createElement('div');
+    projectDisplay.className = 'project';
+
+    const projectButton = document.createElement('button');
+    projectButton.className = 'project-button';
+    projectButton.textContent = `#${project}`;
+    projectButton.addEventListener('click', () => {
+        currentFilter = 'project';
+        currentProject = project;
+        renderCurrentTasks();
+    })
+
+    projectDisplay.appendChild(projectButton);
+    
+    const deleteProjectButton = document.createElement('button');
+    deleteProjectButton.className = 'delete-project';
+    deleteProjectButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete-circle</title><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M17,7H14.5L13.5,6H10.5L9.5,7H7V9H17V7M9,18H15A1,1 0 0,0 16,17V10H8V17A1,1 0 0,0 9,18Z" /></svg>'
+    deleteProjectButton.addEventListener('click', () => {
+        if(confirm('Are you sure?')){
+            taskHandler.removeProject(project, projectDisplay);
+        }
+    });
+    projectDisplay.appendChild(deleteProjectButton);
+
+    projectsListContainer.appendChild(projectDisplay);
+}
+
 if (projectsList){
     projectsList.forEach(project => {
-        const projectDisplay = document.createElement('div');
-        projectDisplay.className = 'project';
-        
-        const projectButton = document.createElement('button');
-        projectButton.className = 'project-button';
-        projectButton.textContent = `#${project}`;
-        projectButton.addEventListener('click', () => {
-            currentFilter = 'project';
-            currentProject = project;
-            renderCurrentTasks();
-        })
-        projectDisplay.appendChild(projectButton);
-
-        const deleteProjectButton = document.createElement('button');
-        deleteProjectButton.className = 'delete-project';
-        deleteProjectButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete-circle</title><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M17,7H14.5L13.5,6H10.5L9.5,7H7V9H17V7M9,18H15A1,1 0 0,0 16,17V10H8V17A1,1 0 0,0 9,18Z" /></svg>'
-        deleteProjectButton.addEventListener('click', () => {
-            if(confirm('Are you sure?')){
-                taskHandler.removeProject(project, projectDisplay);
-            }
-        })
-        projectDisplay.appendChild(deleteProjectButton);
-        
-        projectsListContainer.appendChild(projectDisplay);
+        renderProject(project);
     });
 }
 
@@ -76,11 +81,20 @@ let currentSortDirection = defaultSortDirection;
 let currentProject;
 let currentPriority;
 
-function renderCurrentTasks(){
+function renderCurrentTasks() {
     const taskList = filterTasks(taskHandler, currentFilter, currentProject, currentPriority);
     sortTasks(taskList, currentSort, currentSortDirection);
-
-    renderTasks(taskList, taskHandler);
+    if (Array.isArray(taskList) && taskList.length > 0) {
+        renderTasks(taskList, taskHandler);
+    } else {
+        const taskListContainer = document.querySelector('#task-list-table');
+        taskListContainer.textContent = 'No available tasks.';
+    }
 }
+
+export {
+    renderProject
+}
+
 
 renderCurrentTasks();
